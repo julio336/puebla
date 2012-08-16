7fa9c073fb7b56a594825103838a56b5581be9e1
julio <julio336@hotmail.com>
2012-08-15 23:57:55 -0500

    email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,12 +14,17 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+    		password: "foobar", password_confirmation: "foobar")
   end
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
 
   it { should be_valid }
 
@@ -57,7 +62,7 @@ describe User do
       end      
     end
   end
-
+  
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -65,6 +70,21 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 end
